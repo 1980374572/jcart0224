2439af0c550c0072449cc677e333288944ef4600
baolovestudy <1980374572@qq.com>
2020-03-10 20:15:01 +0800

    add order search ok

--- jcart-administration-back/src/main/java/io/bnn/jcartadministrationback/service/OrderService.java
+++ jcart-administration-back/src/main/java/io/bnn/jcartadministrationback/service/OrderService.java
@@ -1,12 +1,13 @@
 package io.bnn.jcartadministrationback.service;
 
 import com.github.pagehelper.Page;
+import io.bnn.jcartadministrationback.dto.in.OrderSearchInDTO;
 import io.bnn.jcartadministrationback.dto.out.OrderListOutDTO;
 import io.bnn.jcartadministrationback.dto.out.OrderShowOutDTO;
 import io.bnn.jcartadministrationback.po.Order;
 
 public interface OrderService {
-    Page<OrderListOutDTO> search(Integer pageNum);
+    Page<OrderListOutDTO> search(OrderSearchInDTO orderSearchInDTO, Integer pageNum);
 
     OrderShowOutDTO getById(Long orderId);
 
