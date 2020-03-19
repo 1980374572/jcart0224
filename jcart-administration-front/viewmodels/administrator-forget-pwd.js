05aa7ce00ea89eea0cd6fd52225f1350655f34e2
baolovestudy <1980374572@qq.com>
2020-03-19 14:22:37 +0800

    add forget pwd loading

--- jcart-administration-front/viewmodels/administrator-forget-pwd.js
+++ jcart-administration-front/viewmodels/administrator-forget-pwd.js
@@ -1,11 +1,24 @@
 var app = new Vue({
     el: '#app',
     data: {
-        email: ''
+        email: '',
+        loading: false,
+        buttonEnabled: true,
     },
     methods: {
         handleFindBackPwdClick() {
             console.log('find back pwd click');
+            this.loading = true;
+            this.buttonEnabled = false;
+
+            setInterval(function () {
+                console.log('count down');
+                app.counter--;
+                if (app.counter < 0) {
+                    app.buttonEnabled = true;
+                }
+            }, 1000);
+
             this.getPwdResetCode();
         },
         getPwdResetCode() {
@@ -16,6 +29,7 @@ var app = new Vue({
             })
                 .then(function (response) {
                     console.log(response);
+                    app.loading = false;
                     alert('重置码已发送到邮箱');
                 })
                 .catch(function (error) {
