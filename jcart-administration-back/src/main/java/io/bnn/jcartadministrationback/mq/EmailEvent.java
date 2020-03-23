c7fdff3190b0164c5d2e247702dc92e1f5d6fc4e
baolovestudy <1980374572@qq.com>
2020-03-23 15:13:08 +0800

    add jcart suppert back ok

---
+++ jcart-administration-back/src/main/java/io/bnn/jcartadministrationback/mq/EmailEvent.java
@@ -0,0 +1,31 @@
+package io.bnn.jcartadministrationback.mq;
+
+public class EmailEvent {
+    public String toEmail;
+    public String title;
+    public String content;
+
+    public String getToEmail() {
+        return toEmail;
+    }
+
+    public void setToEmail(String toEmail) {
+        this.toEmail = toEmail;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ jcart-support-back/src/test/java/io/bnn/jcartsupportback/jcartsupportback/JcartSupportBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bnn.jcartsupportback.jcartsupportback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcartSupportBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
