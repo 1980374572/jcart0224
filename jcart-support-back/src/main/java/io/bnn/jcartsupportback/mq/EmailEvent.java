832b3131a580474c440eb3adcc2f655acade59bc
baolovestudy <1980374572@qq.com>
2020-03-23 15:27:03 +0800

    add suppert back

---
+++ jcart-support-back/src/main/java/io/bnn/jcartsupportback/mq/EmailEvent.java
@@ -0,0 +1,31 @@
+package io.bnn.jcartsupportback.mq;
+
+public class EmailEvent {
+    public String toEmail;
+    public String title;
+    public String content;
+
+    public String getToEmail() {
+        return toEmail;
+    }
+
+    public void setToEmail(String toEmail) {
+        this.toEmail = toEmail;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ jcart-support-back/src/main/java/io/bnn/jcartsupportback/util/EmailUtil.java
@@ -0,0 +1,33 @@
+package io.bnn.jcartsupportback.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailUtil {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void send(
+            String fromEmail,
+            String email,
+            String title,
+            String content
+    ){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(email);
+        message.setSubject(title);
+        message.setText(content);
+        javaMailSender.send(message);
+        logger.info("send email ended");
+    }
+}
